bb9b6b2628166a312724796f455ae2e22af5edd5
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2026-01-04 23:47:23 +0700

    chore: add neccessary lib

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,15 +1,10 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("convention.android.library")
+    id("convention.android.hilt")
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+
+android {
+    namespace = "com.example.core.network"
 }
 
 dependencies {
@@ -22,5 +17,6 @@ dependencies {
 
     // Inject
     implementation(libs.javax.inject)
+
 
 }
--- core/network/src/main/java/com/example/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/example/core/network/di/NetworkModule.kt
@@ -1,6 +1,12 @@
 package com.example.core.network.di
 
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
+
+@Module
+@InstallIn(SingletonComponent::class)
 internal object NetworkModule {
 
 }
