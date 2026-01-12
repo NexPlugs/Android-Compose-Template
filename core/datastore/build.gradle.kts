ee433142e0bd2e324eeff549966f08ae70c60db1
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2026-01-12 18:12:52 +0700

    chore: add gradle for dataStore module

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("convention.android.library")
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+
+android {
+    namespace = "com.example.core.datastore"
 }
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+
+dependencies {
+    // Kotlin
+    implementation(libs.kotlinx.coroutines.core)
 }
