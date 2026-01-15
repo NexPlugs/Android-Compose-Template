a4a88b09ec005da217206875786f681f8dc1b810
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2026-01-15 14:20:48 +0700

    chore: add some lib for room database

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,8 +1,22 @@
 plugins {
-    id("java-library")
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    id("convention.android.library")
+    id("convention.android.hilt")
 }
 
+
+android {
+    namespace = "com.example.core.database"
+}
+
+dependencies {
+    // Projects
+    implementation(project(":core:model"))
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+}
