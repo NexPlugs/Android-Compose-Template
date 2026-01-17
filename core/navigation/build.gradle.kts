279748116e961db5a504eda80a156384e3c7e562
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2026-01-17 18:24:57 +0700

    chore: update dependency of navigation3

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("convention.android.library")
+    id("convention.android.library.compose")
+    id("convention.android.hilt")
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -9,10 +12,12 @@ android {
 dependencies {
     //Kotlin
     implementation(libs.androidx.core)
-    implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.coroutines.core)
 
+    implementation(libs.kotlinx.serialization.json)
+
     //Navigation3
-    api(libs.androidx.navigation.ui)
-    api(libs.androidx.navigation.compose)
+
+    api(libs.androidx.navigation3.runtime.android)
+    api(libs.androidx.navigation3.ui.android)
 }
