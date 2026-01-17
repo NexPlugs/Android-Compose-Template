0c9828a7781678c76ed7ee37b33714cc5cf4add3
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2026-01-17 17:59:56 +0700

    chore: add dependencies to navigation module

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,13 +1,18 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("convention.android.library")
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+
+android {
+    namespace = "com.example.core.navigation"
 }
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+
+dependencies {
+    //Kotlin
+    implementation(libs.androidx.core)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.coroutines.core)
+
+    //Navigation3
+    api(libs.androidx.navigation.ui)
+    api(libs.androidx.navigation.compose)
 }
